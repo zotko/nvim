56d6c4a5dcd679be16702e326cc49b86d6ed20f2
Mykola Zotko <mzotko@gmail.com>
2025-10-30 10:51:49 +0100

    feat: add Gvdiffsplit key mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,5 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>gd", "<cmd>Gvdiffsplit<CR>", { desc = "Git diff split" })
