c622fa4c025c7e255eaff79277f6998356db2819
Mykola Zotko <mzotko@gmail.com>
2025-10-30 10:52:40 +0100

    refactor: remove redundnat line

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,9 +3,6 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls", "basedpyright" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
-require("nvchad.configs.lspconfig").defaults()
-
 vim.lsp.config("basedpyright", {
     settings = {
         basedpyright = {
